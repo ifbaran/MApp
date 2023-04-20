22cf2889da09b0dda9a47921cfc93deed973d16c
Ibrahim Furkan Baran <baran.ibrahim.izu@gmail.com>
2023-04-20 17:30:26 +0300

    Initial Commit

---
+++ MApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MApp
+//
+//  Created by Baran on 20.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
